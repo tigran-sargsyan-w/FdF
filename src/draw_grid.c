dbb251422e0514a944c6ef3d33a3ac2bbeb8411a
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-03-15 15:20:50 +0100

    -refactor: draw grid func

--- src/draw_grid.c
+++ src/draw_grid.c
@@ -6,50 +6,40 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 12:03:19 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/03/14 22:09:17 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/03/15 15:17:47 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	draw_horizontal_line(int i, int j, t_vars *vars,
-		t_point2d proj_current)
+static void	draw_horizontal_line(int i, int j, t_vars *vars, t_point2d current)
 {
-	t_point2d	proj_next;
+	t_point2d	next;
 
 	if (j < vars->map->columns - 1)
 	{
-		proj_next = vars->map->projected_points[i][j + 1];
-		draw_line(vars, proj_current, proj_next, vars->map->colors[i][j]);
+		next = vars->map->projected_points[i][j + 1];
+		draw_line(vars, current, next, vars->map->colors[i][j]);
 	}
 }
 
-static void	draw_vertical_line(int i, int j, t_vars *vars,
-		t_point2d proj_current)
+static void	draw_vertical_line(int i, int j, t_vars *vars, t_point2d current)
 {
-	t_point2d	proj_next;
+	t_point2d	next;
 
 	if (i < vars->map->rows - 1)
 	{
-		proj_next = vars->map->projected_points[i + 1][j];
-		draw_line(vars, proj_current, proj_next, vars->map->colors[i][j]);
+		next = vars->map->projected_points[i + 1][j];
+		draw_line(vars, current, next, vars->map->colors[i][j]);
 	}
-}
-
-static void	draw_cell_lines(int i, int j, t_vars *vars)
-{
-	t_point2d	proj_current;
-
-	proj_current = vars->map->projected_points[i][j];
-	draw_horizontal_line(i, j, vars, proj_current);
-	draw_vertical_line(i, j, vars, proj_current);
 }
 
 // Function to draw grid
 void	draw_grid(t_vars vars)
 {
-	int				i;
-	int				j;
+	int			i;
+	int			j;
+	t_point2d	point;
 
 	i = 0;
 	while (i < vars.map->rows)
@@ -57,7 +47,9 @@ void	draw_grid(t_vars vars)
 		j = 0;
 		while (j < vars.map->columns)
 		{
-			draw_cell_lines(i, j, &vars);
+			point = vars.map->projected_points[i][j];
+			draw_horizontal_line(i, j, &vars, point);
+			draw_vertical_line(i, j, &vars, point);
 			j++;
 		}
 		i++;
